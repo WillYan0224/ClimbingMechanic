31ce491bd35fac92e7778abd772698243ffbc977
WillYan <yanchung1998@gmail.com>
2025-09-26 02:21:03 +0900

    MultiCapsuleTrace

--- Source/ClimbingMechanic/Private/Components/CustomMovementComponent.cpp
+++ Source/ClimbingMechanic/Private/Components/CustomMovementComponent.cpp
@@ -3,3 +3,34 @@
 
 #include "Components/CustomMovementComponent.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
+void UCustomMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType,
+                                             FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	TraceClimbableSurfaces();
+}
+
+TArray<FHitResult> UCustomMovementComponent::DoMultiCapsuleTraceByObject(FVector Start,FVector End,	bool bShowDebug)
+{
+	TArray<FHitResult> OutCapsuleTraceHitResults;
+	
+	UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+		this,  Start, End, CapsuleTraceRadius, CapsuleTraceHalfHeight,
+		ObjectTypesToTrace, false, TArray<AActor*>(),
+		bShowDebug ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None, OutCapsuleTraceHitResults, false
+	);
+	
+
+	return OutCapsuleTraceHitResults;
+}
+
+void UCustomMovementComponent::TraceClimbableSurfaces()
+{
+	const FVector StartOffset =  UpdatedComponent->GetForwardVector() * 30.f;
+	const FVector Start = UpdatedComponent->GetComponentLocation() + StartOffset;
+	const FVector End = Start + UpdatedComponent->GetForwardVector();
+	DoMultiCapsuleTraceByObject(Start, End, true);
+}
--- Source/ClimbingMechanic/Public/Components/CustomMovementComponent.h
+++ Source/ClimbingMechanic/Public/Components/CustomMovementComponent.h
@@ -13,5 +13,27 @@ UCLASS()
 class CLIMBINGMECHANIC_API UCustomMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
+
+public:
+
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+private:
+
+	// Climb trace
+	TArray<FHitResult> DoMultiCapsuleTraceByObject(FVector Start, FVector End, bool bShowDebug = false);
+
+	void TraceClimbableSurfaces();
+
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterMovement|Climbing")
+	float CapsuleTraceRadius = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterMovement|Climbing")
+	float CapsuleTraceHalfHeight = 80.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterMovement|Climbing")
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypesToTrace;
 	
 };
